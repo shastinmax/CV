d3cf23ee3a975be7d6ee01ef4f35046430a3eebe
Shastin Maksim <shastin_maksim@mail.ru>
2021-12-27 21:09:50 +0300

    start js aside navar

--- js/script.js
+++ js/script.js
@@ -74,3 +74,22 @@ if(event.target===lightboxClose || event.target===lightbox){
     toggleLightBox()
 }
 })
+// Aside NavBar
+const nav=document.querySelector('.nav'),
+navList=nav.querySelectorAll('li'),
+totalNavList=navList.length
+
+for(let i=0;i<totalNavList;i++){
+    const a= navList[i].querySelector('a');
+    a.addEventListener('click',function(){
+        for(let j=0;j<totalNavList;j++){
+            navList[j].querySelector('a').classList.remove('active')
+        }
+       this.classList.add('active')
+
+       showSection(this);
+    })
+}
+function showSection(element){
+    console.log(element.getAttribute('href'));
+}
