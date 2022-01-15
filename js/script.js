faccc0125641c09379bdba1a916e2b76b04d7c0a
Shastin Maksim <shastin_maksim@mail.ru>
2022-01-15 14:56:41 +0300

    preloader

--- js/script.js
+++ js/script.js
@@ -1,126 +1,138 @@
 
 
 // portfolio item filter
-const filterContainer=document.querySelector('.portfolio-filter'),
-lightboxClose=document.querySelector('.lightbox-close'),
-filterBtns=filterContainer.children,
-totalFilterBtn=filterBtns.length
-portfolioItems=document.querySelectorAll('.portfolio-item')
-totalPortfolioItem=portfolioItems.length
+const filterContainer = document.querySelector('.portfolio-filter'),
+    lightboxClose = document.querySelector('.lightbox-close'),
+    filterBtns = filterContainer.children,
+    totalFilterBtn = filterBtns.length
+portfolioItems = document.querySelectorAll('.portfolio-item')
+totalPortfolioItem = portfolioItems.length
 
-for(let i=0;i<totalFilterBtn;i++){
-filterBtns[i].addEventListener('click',function(){
-    filterContainer.querySelector('.active').classList.remove('active')
-    this.classList.add('active')
+for (let i = 0; i < totalFilterBtn; i++) {
+    filterBtns[i].addEventListener('click', function () {
+        filterContainer.querySelector('.active').classList.remove('active')
+        this.classList.add('active')
 
-    const filterValue=this.getAttribute('data-filter')
-    for(let k=0;k<totalPortfolioItem;k++){
-        if(filterValue===portfolioItems[k].getAttribute('data-category')){
-            portfolioItems[k].classList.remove('hide')
-            portfolioItems[k].classList.add('show')
-        }else{
-            portfolioItems[k].classList.remove('show')
-            portfolioItems[k].classList.add('hide')
+        const filterValue = this.getAttribute('data-filter')
+        for (let k = 0; k < totalPortfolioItem; k++) {
+            if (filterValue === portfolioItems[k].getAttribute('data-category')) {
+                portfolioItems[k].classList.remove('hide')
+                portfolioItems[k].classList.add('show')
+            } else {
+                portfolioItems[k].classList.remove('show')
+                portfolioItems[k].classList.add('hide')
+            }
+            if (filterValue === 'all') {
+                portfolioItems[k].classList.remove('hide')
+                portfolioItems[k].classList.add('show')
+            }
         }
-        if(filterValue==='all'){
-            portfolioItems[k].classList.remove('hide')
-            portfolioItems[k].classList.add('show') 
-        }
-    }
-})
+    })
 }
 
 // portfolio lightbox
 
-const lightbox=document.querySelector('.lightbox'),
-lightboxImg=lightbox.querySelector('.lightbox-img'),
-lightboxText=lightbox.querySelector('.caption-text'),
-lightboxCounter=lightbox.querySelector('.caption-counter')
+const lightbox = document.querySelector('.lightbox'),
+    lightboxImg = lightbox.querySelector('.lightbox-img'),
+    lightboxText = lightbox.querySelector('.caption-text'),
+    lightboxCounter = lightbox.querySelector('.caption-counter')
 
-let itemIndex=0
+let itemIndex = 0
 
-for(let i=0;i<totalPortfolioItem;i++){
-    portfolioItems[i].addEventListener('click',function(){
-        itemIndex=i
+for (let i = 0; i < totalPortfolioItem; i++) {
+    portfolioItems[i].addEventListener('click', function () {
+        itemIndex = i
         changeItem()
         toggleLightBox()
     })
 }
 
-function nextItem(){
-    itemIndex===totalPortfolioItem - 1 ? itemIndex = 0 : itemIndex++
-    changeItem()  
+function nextItem() {
+    itemIndex === totalPortfolioItem - 1 ? itemIndex = 0 : itemIndex++
+    changeItem()
 }
 
-function prevItem(){
-    itemIndex===0 ? itemIndex=totalPortfolioItem - 1 : itemIndex--
-    changeItem()  
+function prevItem() {
+    itemIndex === 0 ? itemIndex = totalPortfolioItem - 1 : itemIndex--
+    changeItem()
 }
 
-function toggleLightBox(){
+function toggleLightBox() {
     lightbox.classList.toggle('open')
 }
 
-function changeItem(){
-    imgSrc=portfolioItems[itemIndex].querySelector('.portfolio-img img').getAttribute('src')
-    lightboxImg.src=imgSrc
-    lightboxText.innerHTML=portfolioItems[itemIndex].querySelector("h4").innerHTML
-    lightboxCounter.innerHTML=(itemIndex+1) + " of " + totalPortfolioItem
+function changeItem() {
+    imgSrc = portfolioItems[itemIndex].querySelector('.portfolio-img img').getAttribute('src')
+    lightboxImg.src = imgSrc
+    lightboxText.innerHTML = portfolioItems[itemIndex].querySelector("h4").innerHTML
+    lightboxCounter.innerHTML = (itemIndex + 1) + " of " + totalPortfolioItem
 }
 
 // close lightbox
-lightbox.addEventListener('click',function(event){
-if(event.target===lightboxClose || event.target===lightbox){
-    toggleLightBox()
-}
+lightbox.addEventListener('click', function (event) {
+    if (event.target === lightboxClose || event.target === lightbox) {
+        toggleLightBox()
+    }
 })
 // Aside NavBar
-const nav=document.querySelector('.nav'),
-navList=nav.querySelectorAll('li'),
-totalNavList=navList.length,
-allSection=document.querySelectorAll('.section'),
-totalSection=allSection.length
+const nav = document.querySelector('.nav'),
+    navList = nav.querySelectorAll('li'),
+    totalNavList = navList.length,
+    allSection = document.querySelectorAll('.section'),
+    totalSection = allSection.length
 
-for(let i=0;i<totalNavList;i++){
-    const a= navList[i].querySelector('a');
-    a.addEventListener('click',function(){
+for (let i = 0; i < totalNavList; i++) {
+    const a = navList[i].querySelector('a');
+    a.addEventListener('click', function () {
         for (let i = 0; i < totalSection; i++) {
             allSection[i].classList.remove('back-section')
         }
-        for(let j=0;j<totalNavList;j++){
-            if(navList[j].querySelector('a').classList.contains('active')){
+        for (let j = 0; j < totalNavList; j++) {
+            if (navList[j].querySelector('a').classList.contains('active')) {
 
-    allSection[j].classList.add('back-section')
-}
+                allSection[j].classList.add('back-section')
+            }
             navList[j].querySelector('a').classList.remove('active')
 
         }
-       this.classList.add('active')
-       showSection(this);
+        this.classList.add('active')
+        showSection(this);
 
-       if(window.innerWidth<1200){
-           asideSectiontogglerBtn()
-       }
+        if (window.innerWidth < 1200) {
+            asideSectiontogglerBtn()
+        }
     })
 }
-function showSection(element){
+function showSection(element) {
     for (let i = 0; i < totalSection; i++) {
         allSection[i].classList.remove('active')
     }
-    const target=element.getAttribute('href').split('#')[1]
-    document.querySelector('#'+target).classList.add('active')
-    
+    const target = element.getAttribute('href').split('#')[1]
+    document.querySelector('#' + target).classList.add('active')
+
 }
 
-const navTogglerBtn=document.querySelector('.nav-toggler'),
-        aside=document.querySelector('.aside')
+const navTogglerBtn = document.querySelector('.nav-toggler'),
+    aside = document.querySelector('.aside')
 
-navTogglerBtn.addEventListener('click',asideSectiontogglerBtn)
+navTogglerBtn.addEventListener('click', asideSectiontogglerBtn)
 
-function asideSectiontogglerBtn(){
+function asideSectiontogglerBtn() {
     aside.classList.toggle('open');
     navTogglerBtn.classList.toggle('open');
     for (let i = 0; i < totalSection; i++) {
         allSection[i].classList.toggle('open')
     }
 }
+//40.12
+
+
+
+const arr = [2, 3, 1, 3, 2, 5, 6, 7, 7]
+arr.reduce((acc, num) => {
+    const keys = Object.keys(acc)
+    if (keys.find(num)) {
+        acc[num] += 1
+    } else { acc[num] = 1 }
+    return acc
+}, {})
