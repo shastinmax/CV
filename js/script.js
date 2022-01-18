2997a0c61429780d518243ba5537f29546adae93
Shastin Maksim <shastin_maksim@mail.ru>
2022-01-18 21:47:25 +0300

    add scripm hire me

--- js/script.js
+++ js/script.js
@@ -90,13 +90,12 @@ const nav = document.querySelector('.nav'),
 for (let i = 0; i < totalNavList; i++) {
     const a = navList[i].querySelector('a');
     a.addEventListener('click', function () {
-        for (let i = 0; i < totalSection; i++) {
-            allSection[i].classList.remove('back-section')
-        }
+        removeBackSectionClass()
+       
         for (let j = 0; j < totalNavList; j++) {
             if (navList[j].querySelector('a').classList.contains('active')) {
-
-                allSection[j].classList.add('back-section')
+                addBackSectionClass(j)
+                
             }
             navList[j].querySelector('a').classList.remove('active')
 
@@ -109,6 +108,14 @@ for (let i = 0; i < totalNavList; i++) {
         }
     })
 }
+function removeBackSectionClass(){
+    for(let i=0;i<totalNavList;i++){
+        allSection[i].classList.remove('back-section')
+    }
+}
+function addBackSectionClass(num){
+allSection[num].classList.add('back-section')
+}
 function showSection(element) {
     for (let i = 0; i < totalSection; i++) {
         allSection[i].classList.remove('active')
@@ -117,6 +124,26 @@ function showSection(element) {
     document.querySelector('#' + target).classList.add('active')
 
 }
+function updateNav(element){
+    for(let i=0;i<totalNavList;i++){
+        navList[i].querySelector('a').classList.remove('active')
+      const target=element.getAttribute('href').split('#')[1]
+      if(target===navList[i].querySelector('a').getAttribute('href').split('#')[1]){
+        navList[i].querySelector('a').classList.add('active')
+      }
+    }
+    
+
+}
+
+document.querySelector('.hire-me').addEventListener('click',function(){
+    const sectionIndex=this.getAttribute('data-section-index')
+    // console.log(sectionIndex)
+    showSection(this)
+    updateNav(this)
+    removeBackSectionClass()
+    addBackSectionClass(sectionIndex)
+})
 
 const navTogglerBtn = document.querySelector('.nav-toggler'),
     aside = document.querySelector('.aside')
@@ -134,11 +161,11 @@ function asideSectiontogglerBtn() {
 
 
 
-const arr = [2, 3, 1, 3, 2, 5, 6, 7, 7]
-arr.reduce((acc, num) => {
-    const keys = Object.keys(acc)
-    if (keys.find(num)) {
-        acc[num] += 1
-    } else { acc[num] = 1 }
-    return acc
-}, {})
+// const arr = [2, 3, 1, 3, 2, 5, 6, 7, 7]
+// arr.reduce((acc, num) => {
+//     const keys = Object.keys(acc)
+//     if (keys.find(num)) {
+//         acc[num] += 1
+//     } else { acc[num] = 1 }
+//     return acc
+// }, {})
